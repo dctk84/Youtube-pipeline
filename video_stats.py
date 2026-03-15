5dae8ec043cdb2b23b838a7a2dcfab12a2217949
dctk84 <dinhcongtrungkien84@gmail.com>
2026-03-16 00:10:39 +0700

    Update video_stats.py file

--- video_stats.py
+++ video_stats.py
@@ -7,6 +7,7 @@ load_dotenv(dotenv_path="./.env")
 
 api_key = os.getenv('api_key')
 channel_handle = "MrBeast"
+maxRessults = 50
 
 def get_playlist_id():
     try:
@@ -23,12 +24,51 @@ def get_playlist_id():
 
         channel_items = data["items"][0]
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
-        print(channel_playlistId)
+        # print(channel_playlistId)
 
         return channel_playlistId
     
     except requests.exceptions.RequestException as e:
         raise e
+
+
+def get_video_ids(playlistId):
     
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxRessults}&playlistId={playlistId}&key={api_key}"
+
+    try:
+
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+        
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
