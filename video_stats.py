bd550d083a9c99e185cba958791fc0b6bf04d911
dctk84 <dinhcongtrungkien84@gmail.com>
2026-03-16 23:47:24 +0700

    Update main script

--- video_stats.py
+++ video_stats.py
@@ -2,12 +2,13 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 
 api_key = os.getenv('api_key')
 channel_handle = "MrBeast"
-maxRessults = 50
+maxResults = 50
 
 def get_playlist_id():
     try:
@@ -38,7 +39,7 @@ def get_video_ids(playlistId):
 
     pageToken = None
 
-    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxRessults}&playlistId={playlistId}&key={api_key}"
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={api_key}"
 
     try:
 
@@ -69,6 +70,57 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def batch_list(video_id_lst, batch_size):
+    for video_id in range(0, len(video_id_lst), batch_size):
+        yield video_id_lst[video_id: video_id + batch_size]
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    try:
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={api_key}'
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None),
+                    "likeCount": statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None)
+                }
+
+                extracted_data.append(video_data)
+        
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
-    get_video_ids(playlistId)
+    video_ids = get_video_ids(playlistId)
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
